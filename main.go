58d63d5d7c21d951dd15d511d2192abe7cd7f848
Lokesh Konka <konkalokesh372@gmail.com>
2025-11-18 17:33:45 +0530

    /api/todos post route

--- main.go
+++ main.go
@@ -6,14 +6,33 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 )
-
+type Todo struct {
+	ID 		int		`json:"id"` 
+	Completed bool 	`json:"completed"`
+	Body 	string `json:"body"`
+}
 func main(){
 	
 	fmt.Println("Hello !")
 
 	app:= fiber.New()
+
+
+	todos := []Todo{}
 	app.Get("/",func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"mesge": "This is the  / route"})
+	})
+	app.Post("/api/todos",func (c *fiber.Ctx) error {
+		todo:= &Todo{}
+		if err:= c.BodyParser(todo) ; err!=nil {
+			return err
+		}
+		if todo.Body == ""{
+			return c.Status(400).JSON(fiber.Map{"error":"Your todo body is empty "})
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.Status(201).JSON(todo)
 	})
 
 
